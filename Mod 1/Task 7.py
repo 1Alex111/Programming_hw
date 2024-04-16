6a5c211888a7ebaaa3c478f0eceafd3bdd63ba39
1Alex111 <strelkov.1999@list.ru>
2024-04-17 00:34:03 +0500

    Create Task 7.py

---
+++ Mod 1/Task 7.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+visit_counter = 0
+
+@app.route('/counter')
+def counter():
+    global visit_counter
+    visit_counter += 1
+    return f'{visit_counter}'
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
